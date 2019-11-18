fcb6d87de8bcfb3279bc04159117c221863d2241
jessicagamio <jessica.gamio@gmail.com>
2019-11-18 15:40:57 -0800

    setup unit test for code challenge

---
+++ addll.py
@@ -0,0 +1,55 @@
+
+import unittest
+
+class Node(object):
+    """Linked list node."""
+
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def as_rev_string(self):
+        """Represent data for this node and its successors as a string.
+
+        >>> l1 = Node(3)
+        >>> l1.as_rev_string()
+        '3'
+
+        >>> l1 = Node(3, Node(2, Node(1)))
+        >>> l1.as_rev_string()
+        '123'
+        """
+
+        out = []
+        n = self
+
+        while n:
+            out.append(str(n.data))
+            n = n.next
+
+        return "".join(reversed(out))
+
+
+def add_linked_lists(l1, l2):
+    """Given two linked lists, treat like numbers and add together.
+
+    l1: the head node of a linked list in "reverse-digit" format
+    l2: the head node of another "reverse-digit" format
+
+    Returns: head node of linked list of sum in "reverse-digit" format.
+    """
+
+
+
+
+l1 = Node(3, Node(2, Node(1)))
+l2 = Node(4, Node(3, Node(2)))
+
+
+class TestFunction(unittest.TestCase):
+    def test_addll(self):
+        self.assertEqual(add_linked_lists(l1,l2).as_rev_string(),"357")
+
+
+if __name__=='__main__':
+    unittest.main()
