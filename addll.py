01e42fbd0594880e1106d4472036d14540433902
jessicagamio <jessica.gamio@gmail.com>
2019-11-18 16:30:55 -0800

    changed addNode function removed Linked List class

--- addll.py
+++ addll.py
@@ -1,12 +1,43 @@
 
 import unittest
 
+
+
+# class LinkedList(object):
+#     """create linked list"""
+
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+
+#     def addNode(self, node):
+
+#         if self.head == None:
+#             self.head = node
+#         if self.tail !=None:
+#             self.tail.next = node
+#         self.tail = node
+
 class Node(object):
     """Linked list node."""
 
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
+
+    def addNode(self,data):
+        node=Node(data)
+
+        if self.next == None:
+            self.next = node
+
+        else:
+            while self.next != None:
+                self = self.next
+
+            self.next = node
+
+
 
     def as_rev_string(self):
         """Represent data for this node and its successors as a string.
@@ -37,13 +68,43 @@ def add_linked_lists(l1, l2):
     l2: the head node of another "reverse-digit" format
 
     Returns: head node of linked list of sum in "reverse-digit" format.
-    """
+    # """
+
+    newLL=None
+
+    while l1 != None or l2 != None:
+        nodeSum = l1.data + l2.data
+
+        if newLL== None:
+            newLL=Node(nodeSum)
+
+        else:
+            newLL.addNode(nodeSum)
+
+   
+
+        l1 = l1.next
+        l2 = l2.next
+
+    # curr = newLL
+    # while curr != None:
+    #     print(curr.data)
+    #     curr=curr.next
+
+    return newLL
+
+
+   
+
+
 
 
 
 
 l1 = Node(3, Node(2, Node(1)))
 l2 = Node(4, Node(3, Node(2)))
+
+
 
 
 class TestFunction(unittest.TestCase):
